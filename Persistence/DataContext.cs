4a7c2ab163c03574bb4842d4ac5250e468e7d5e8
Aleksandar Markovic <amarkovic@firstbeatmedia.com>
2019-09-18 23:10:04 +0200

    Initial commit

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+  public class DataContext : DbContext
+  {
+
+    public DataContext(DbContextOptions options) : base(options)
+    {
+
+    }
+    public DbSet<Value> Values { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Value>()
+      .HasData(
+        new Value { Id = 1, Name = "pera" },
+        new Value { Id = 2, Name = "pera" },
+        new Value { Id = 3, Name = "pera" }
+      );
+    }
+
+  }
+}
