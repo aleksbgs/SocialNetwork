4b12e8978be4c10005d2c2f1bac6a3384fb2e75d
Aleksandar Markovic <amarkovic@firstbeatmedia.com>
2019-09-20 12:50:27 +0200

    Refactor

---
+++ Persistence/Seed.cs
@@ -0,0 +1,15 @@
+using System.Linq;
+
+namespace Persistence
+{
+  public class Seed
+  {
+    public static void SeedData(DataContext context)
+    {
+      if (!context.Activities.Any())
+      {
+
+      }
+    }
+  }
+}
