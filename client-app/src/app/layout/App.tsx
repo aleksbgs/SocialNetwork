6a82049183feafc74a05251ea9b4dc9a820bc28d
Aleksandar Markovic <amarkovic@firstbeatmedia.com>
2019-09-24 20:22:55 +0200

    mobx

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,77 +1,41 @@
-import React, { useState, useEffect, Fragment,SyntheticEvent } from 'react'
+import React, { useState, useEffect, Fragment, SyntheticEvent, useContext } from 'react'
 import { Container } from 'semantic-ui-react';
 import { IActivity } from '../models/activity';
 import NavBar from '../../features/nav/NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import agent from '../api/agent';
 import LoadingComponent from './LoadingComponent';
+import ActivityStore from '../stores/activityStore';
+import { observer } from 'mobx-react-lite';
 
 const App = () => {
+  const activityStore = useContext(ActivityStore);
   const [activities, setActivities] = useState<IActivity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(null);
   const [editMode, setEditMode] = useState(false);
   const [loading, setLoading] = useState(true);
   const [submiting, setSubmitting] = useState(false);
-  const [target,setTarget] = useState('');
+  const [target, setTarget] = useState('');
 
-  const handleSelectActivity = (id: string) => {
-    setSelectedActivity(activities.filter(a => a.id === id)[0]);
-    setEditMode(false);
-  }
-  const handleOpenCreateForm = () => {
-    setSelectedActivity(null);
-    setEditMode(true);
-  }
-  const handleCreateActivity = (activity: IActivity) => {
-    setSubmitting(true);
-    agent.Activites.create(activity).then(() => {
-      setActivities([...activities, activity]);
-      setSelectedActivity(activity);
-      setEditMode(false);
-    }).then(() => setSubmitting(false));
-  }
-  const handleEditActivity = (activity: IActivity) => {
-    setSubmitting(true);
-    agent.Activites.update(activity).then(() => {
-      setActivities([...activities.filter(a => a.id !== activity.id), activity])
-      setSelectedActivity(activity);
-      setEditMode(false);
-    }).then(() => setSubmitting(false));
-  }
-  const handleDeleteActivity = (event: SyntheticEvent<HTMLButtonElement>, id: string)  => {
+
+  const handleDeleteActivity = (event: SyntheticEvent<HTMLButtonElement>, id: string) => {
     setSubmitting(true);
     setTarget(event.currentTarget.name);
-    agent.Activites.delete(id).then(() => {
+    agent.Activities.delete(id).then(() => {
       setActivities([...activities.filter(a => a.id !== id)])
     }).then(() => setSubmitting(false));
   }
 
   useEffect(() => {
-    agent.Activites.list()
-      .then(response => {
-        let activities: IActivity[] = [];
-        response.forEach((activity) => {
-          activity.date = activity.date.split('.')[0]
-          activities.push(activity)
-        });
-        setActivities(activities)
-      }).then(() => setLoading(false));
-  }, []);
-  if (loading) return <LoadingComponent content='Loading activities...' />
+    activityStore.loadActivities();
+  }, [activityStore]);
+  if (activityStore.loadingInitial) return <LoadingComponent content='Loading activities...' />
 
   return (
     <Fragment>
-      <NavBar openCreateForm={handleOpenCreateForm} />
+      <NavBar />
       <Container style={{ marginTop: '7em' }}>
         <ActivityDashboard
-          activities={activities}
-          selectActivity={handleSelectActivity}
-          selectedActivity={selectedActivity}
-          editMode={editMode}
-          setEditMode={setEditMode}
-          setSelectedActivity={setSelectedActivity}
-          createActivity={handleCreateActivity}
-          editActivity={handleEditActivity}
           deleteActivity={handleDeleteActivity}
           submitting={submiting}
           target={target}
@@ -81,5 +45,5 @@ const App = () => {
   );
 }
 
-export default App
+export default observer(App);
 
---
+++ client-app/src/app/stores/activityStore.tsx
@@ -0,0 +1,79 @@
+import { observable, action, computed } from 'mobx';
+import { createContext } from 'react';
+import { IActivity } from '../models/activity';
+import agent from '../api/agent';
+
+class ActivityStore {
+  @observable activityRegistry = new Map();
+  @observable activities: IActivity[] = [];
+  @observable selectedActivity: IActivity | undefined;
+  @observable loadingInitial = false;
+  @observable editMode = false;
+  @observable submitting = false;
+
+  @computed get activitiesByDate() {
+    return Array.from(this.activityRegistry.values()).sort((a, b) => Date.parse(a.date) - Date.parse(b.date));
+  }
+
+  @action loadActivities = async () => {
+    this.loadingInitial = true;
+    try {
+      const activities = await agent.Activities.list();
+      activities.forEach((activity) => {
+        activity.date = activity.date.split('.')[0]
+        this.activityRegistry.set(activity.id, activity);
+      });
+      this.loadingInitial = false;
+    } catch (error) {
+      console.log(error);
+      this.loadingInitial = false;
+    }
+  };
+  @action createActivity = async (activity: IActivity) => {
+    this.submitting = true;
+    try {
+      await agent.Activities.create(activity);
+      this.activityRegistry.set(activity.id, activity);
+      this.editMode = false;
+      this.submitting = false;
+    } catch (error) {
+      this.submitting = false;
+      console.log(error);
+    }
+  }
+  @action editActivity = async (activity: IActivity) => {
+    try {
+      await agent.Activities.update(activity);
+      this.activityRegistry.set(activity.id, activity);
+      this.selectedActivity = activity;
+      this.editMode = false;
+      this.submitting = false;
+
+    } catch (error) {
+      this.submitting = false;
+      console.log(error);
+    }
+  }
+  @action openCreateForm = () => {
+    this.editMode = true;
+    this.selectedActivity = undefined;
+  }
+  @action openEditForm = (id: string) => {
+    this.selectedActivity = this.activityRegistry.get(id);
+    this.editMode = true;
+  }
+  @action cancelFormOpen = () => {
+    this.editMode = false;
+  }
+  @action cancelSelectActivity = () => {
+    this.selectedActivity = undefined;
+  }
+
+  @action selectActivity = (id: string) => {
+    this.selectedActivity = this.activityRegistry.get(id);
+    this.editMode = false;
+  }
+
+}
+
+export default createContext(new ActivityStore())
